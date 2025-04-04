cb97ae2b0106bf62ba589d67eb15e7f453ee14e2
Jitendra Singh <04jitendrasingh@gmail.com>
2025-04-05 01:18:33 +0530

    Added Terraform files for Medusa ECS deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+﻿variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+variable "ecs_cluster_name" {
+  description = "Name of ECS cluster"
+  default     = "medusa-cluster"
+}
+variable "service_name" {
+  description = "Name of ECS service"
+  default     = "medusa-service"
+}
+variable "container_port" {
+  description = "Container Port"
+  default     = 9000
+}
