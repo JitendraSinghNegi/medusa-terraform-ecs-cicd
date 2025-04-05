0b2315ef958118bf3b87f657c1e5124b90b47234
Jitendra Singh <04jitendrasingh@gmail.com>
2025-04-05 21:51:47 +0530

    Fix: Proper UTF-8 encoding for provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,11 @@
-﻿provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
 }
